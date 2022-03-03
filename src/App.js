77599d381b1a3d8cf7e1cac431911b0dc3b9efcd
obaloluwa28 <oduyemi.obaloluwa@gmail.com>
2022-03-03 14:09:34 +0100

    added side project

--- src/App.js
+++ src/App.js
@@ -9,9 +9,12 @@ import backend from './asset/Images/Backend.png'
 import IOT from './asset/Images/IoT.png'
 import vector from './asset/Images/vector.png'
 import vector_ from './asset/Images/vector1.png'
+import vector2 from './asset/Images/vector2.png'
+import vector2_ from './asset/Images/vector2_.png'
 import mypix from './asset/Images/myPix.png'
-import Footer from './Components/Footer/Footer';
-
+import OurLogo1 from './asset/Images/logo1.png'
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+import { FaTwitterSquare } from "react-icons/fa";
 const App = () => {
   const [bgcolor, setBgcolor] = useState(true)
   const toggleState = () =>{
@@ -39,6 +42,11 @@ const App = () => {
               <div id={bgcolor ? "p2" : "p2_"}>ODUYEMI OBALOLUWA</div>
               <div id={bgcolor ? "p3" : "p3_"}>FULL STACK WEB DEVELOPER</div>
               <div id={bgcolor ? "p4" : "p4_"}><span>Download Resume</span></div>
+              <div className="icon-box">
+                <BsGithub id={bgcolor ? "icons" : "icons_"}/>
+                <FaTwitterSquare id={bgcolor ? "icons" : "icons_"}/>
+                <BsLinkedin id={bgcolor ? "icons" : "icons_"}/>
+              </div>
             </div>
           </div>
 
@@ -128,9 +136,71 @@ const App = () => {
             Hi
           </div>
         </div>
+
+        <div className="buttom_side_proj">
+          <div className="bsp_mini_container">
+            <div className={bgcolor ? "bsp_mini" : "bsp_mini_"}>
+              <span id="sub_tag">Face Recognition Attendance Monitoring System</span>
+              <span id="descptn">The project scans the face of student and marks him/her present for certain course</span>
+              <div id="bps_mini">
+                <span id="ldemo">Live Demo</span>
+                <BsGithub id="icons"/>
+              </div>
+            </div>
+
+            <div className={bgcolor ? "bsp_mini" : "bsp_mini_"}>
+              <span id="sub_tag">EMS Energy Management System</span>
+              <span id="descptn">The System Captures New Stocks, Knows the available stocks in the system, activates Point of Sales and View Transaction Records over a period</span>
+              <div id="bps_mini">
+                <span id= "ldemo">Live Demo</span>
+                <BsGithub id="icons"/>
+              </div>
+            </div>
+
+            <div className={bgcolor ? "bsp_mini" : "bsp_mini_"}>
+              <span id="sub_tag">Inventory Management System</span>
+              <span id="descptn">The application track your goods throughout your entire supply chain, from purchasing to production to end sales. The application also keeps record which can be used to prepare various books of account</span>
+              <div id="bps_mini1">
+                <span id="ldemo">Live Demo</span>
+                <BsGithub id="icons"/>
+              </div>
+            </div>
+
+            <div className={bgcolor ? "bsp_mini" : "bsp_mini_"}>
+              <span id="sub_tag">Ecommerce System</span>
+              <span id="descptn">New Employee into an oragnization is captured into the system and attendance, performance, PayRoll System and evaluation of employee is done on system</span>
+              <div id="bps_mini">
+                <span id= "ldemo">Live Demo</span>
+                <BsGithub id="icons"/>
+              </div>
+            </div>
+
+            <div className={bgcolor ? "bsp_mini" : "bsp_mini_"}>
+              <span id="sub_tag">Employee Management System</span>
+              <span id="descptn">New Employee into an oragnization is captured into the system and attendance, performance, PayRoll System and evaluation of employee is done on system</span>
+              <div id="bps_mini">
+                <span id="ldemo">Live Demo</span>
+                <BsGithub id="icons"/>
+              </div>
+            </div>
+
+            <div className={bgcolor ? "bsp_mini" : "bsp_mini_"}>
+              <span id="sub_tag">IoT Home Automation System</span>
+              <span id="descptn">The Project is aimed at controlling ones remote facility and Home Utilities on a button click. One can also view the facility remotely with IP Camera installed</span>
+              <div id="bps_mini">
+                <span id="ldemo">Live Demo</span>
+                <BsGithub id="icons"/>
+              </div>
+            </div>
+          </div>
+          {bgcolor ? <img src={vector2} alt="vector2"/>:<img src={vector2_} alt="vector2"/>}
+        </div>
       </div>
 
-      <Footer />
+      <div className={bgcolor ? "foorter-container" : "foorter-container_"}>
+        <img src={OurLogo1} alt="Logo" />
+        <span className="copyright">Designed by Oduyemi Obaloluwa</span>
+      </div>
     </div>
   );
 }
