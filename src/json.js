ba502cb74e37f42082c574f05ae1351ee060b87d
obaloluwa28 <oduyemi.obaloluwa@gmail.com>
2022-12-22 15:20:45 +0100

    changed directories

--- src/json.js
+++ src/json.js
@@ -5,33 +5,33 @@ const JsonData = [
         content: "The Project is aimed at controlling ones remote facility and Home Utilities on a button click. One can also view the facility remotely with IP Camera installed",
         imgurl: "https://img.freepik.com/free-vector/smart-house-remote-control-cartoon_33099-2040.jpg?size=626&ext=jpg&ga=GA1.2.107027158.1657193171",
         github: "https://www.cryptosmartnow.io/wp-content/uploads/2022/06/Borderless-Dev-Hub-1024x530.jpg",
-        liveurl: "https://iot-home-automate.herokuapp.com/"
+        liveurl: "https://63a45cba51a71e23b69d9dde--zesty-alfajores-c53f9b.netlify.app/"
     },{
         id: 2,
         jtitle: "EMS Energy Management System",
         content: "The application helps real-estate administrators captures readings from smart Energy, water and gas meters of Tenants and displays consumption on the dashboard, meter can be remotely controlled and vend for on the system.",
         imgurl: "https://img.freepik.com/free-vector/internet-things-iot-smart-connection-control-device-network-industry-resident-anywhere-anytime-anybody-any-business-with-internet-it-technology-futuristic-world_1150-61172.jpg?size=626&ext=jpg&uid=R75165962&ga=GA1.2.107027158.1657193171",
         github: "https://www.cryptosmartnow.io/wp-content/uploads/2022/06/Borderless-Dev-Hub-1024x530.jpg",
-        liveurl: "https://sterling-smart-meter.herokuapp.com/"
+        liveurl: "https://63a460166fbc162ad244c849--thriving-duckanoo-29cf4c.netlify.app/"
     },{
         id: 3,
         jtitle: "Ecommerce Store",
         content: "An E-commerce platform where Items can be pruchased a and orders can be made",
         imgurl: "https://img.freepik.com/premium-vector/3d-online-shopping-social-media-mobile-applications-websites-concepts_131114-30.jpg?size=626&ext=jpg&ga=GA1.2.107027158.1657193171",
         github: "https://www.cryptosmartnow.io/wp-content/uploads/2022/06/Borderless-Dev-Hub-1024x530.jpg",
-        liveurl: "https://ktt-e-commerce-store.herokuapp.com/"
-    },{
-        id: 4,
-        jtitle: "Biometric - Face Recognition for Employee Management ",
-        content: "New Employee into an oragnization is captured into the system and attendance, performance, PayRoll System and evaluation of employee is done on system",
-        imgurl: "https://img.freepik.com/free-vector/biometric-identification-scanners-background_1284-26513.jpg?t=st=1657809342~exp=1657809942~hmac=0fa2d31ba0e373cc5a9f9e36597f1376dbbfb192de1205bac438b6065a2e12de&w=740",
-        github: "https://www.cryptosmartnow.io/wp-content/uploads/2022/06/Borderless-Dev-Hub-1024x530.jpg",
-        liveurl: "https://inventory-systm.herokuapp.com/"
+        liveurl: "https://incandescent-gnome-30af84.netlify.app/"
     },{
         id: 5,
         jtitle: "EMR Software",
         content: "An Electronic Medical Record Web application for managing Doctor Patient Relationship",
         imgurl: "https://img.freepik.com/free-photo/inventory-stock-manufacturing-assets-goods-concept_53876-133673.jpg?t=st=1657851329~exp=1657851929~hmac=d853071735823efae9f2417071041ef26f12719e6c72da4b1949fa0798b206c4&w=740",
+        github: "https://www.cryptosmartnow.io/wp-content/uploads/2022/06/Borderless-Dev-Hub-1024x530.jpg",
+        liveurl: "https://magenta-cat-ccde1e.netlify.app/"
+    },{
+        id: 4,
+        jtitle: "Biometric - Face Recognition for Employee Management ",
+        content: "New Employee into an oragnization is captured into the system and attendance, performance, PayRoll System and evaluation of employee is done on system",
+        imgurl: "https://img.freepik.com/free-vector/biometric-identification-scanners-background_1284-26513.jpg?t=st=1657809342~exp=1657809942~hmac=0fa2d31ba0e373cc5a9f9e36597f1376dbbfb192de1205bac438b6065a2e12de&w=740",
         github: "https://www.cryptosmartnow.io/wp-content/uploads/2022/06/Borderless-Dev-Hub-1024x530.jpg",
         liveurl: "https://inventory-systm.herokuapp.com/"
     }
